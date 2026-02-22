8d01848444402cfe3cac1d5807733e0b7d24c085
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-02-22 14:49:11 -0700

    Add project files.

---
+++ RollOfTheDice/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace RollOfTheDice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random diceRoll = new Random();
+            int[] rollCount = new int[13];
+            string padding = " |";
+
+            Console.WriteLine("         Two six sided dice rolled 1000 times");
+            
+            //create counting array and collect data
+            for (int i = 0; i < 1000; i++)
+            {
+                int rolledDice = diceRoll.Next(2, 13);
+                rollCount[rolledDice]++;
+
+            }
+
+            //Print header 
+            for (int i = 2; i < 13; i++)
+            { 
+                Console.Write($"{i}".PadLeft(3) + padding);
+            }
+            
+            Console.WriteLine();
+            
+            //Print line separator 
+            for (int i = 0; i < 55; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.WriteLine();
+
+            //Print data 
+            for (int i = 2; i < 13; i++)
+            {
+                Console.Write($"{rollCount[i]}".PadLeft(3) + padding);
+            }
+
+            //Pause
+            Console.Read();
+        }
+    }
+}
